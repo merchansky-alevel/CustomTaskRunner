7aa3c7ce689c0dd57476752c6051148250a477bf
IrynaNikerinaGentlecode <iryna.nikerina@gentlecode.com>
2018-08-07 13:18:13 +0300

    UI: in progress

--- CustomTaskRunner/CustomTaskRunner/Program.cs
+++ CustomTaskRunner/CustomTaskRunner/Program.cs
@@ -1,16 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CustomTaskRunner
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("111");
-        }
-    }
+	class Program
+	{
+	static void Main(string[] args)
+		{
+			Console.WriteLine("Welcome to Custom Task Runner!");
+
+			Console.WriteLine("Please, select Task name:");
+			Console.WriteLine("1-Bug");
+			Console.WriteLine("2-Feature");
+			Console.WriteLine("3-Technical Dept");
+			string name = Console.ReadLine();
+
+			Console.WriteLine("Please, select Task complexity from 1 to 5:");
+			string complexity = Console.ReadLine();
+			Sprint sprint = new Sprint();
+			Console.ReadKey();
+		}
+	}
 }
---
+++ CustomTaskRunner/TasksRepository/TaskItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TasksRepository
+{
+	public class TaskItem
+	{
+		public string Name { get; }
+		public string Priority { get; }
+		public string Complexity { get;  }
+
+		public TaskItem(string name, string priora, string coplex)
+		{
+			this.Name = name;
+			this.Priority = priora;
+			this.Complexity = coplex;
+		}
+	}
+}
