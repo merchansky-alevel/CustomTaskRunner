00786604be139f3a16afe9a6ed44a4231b1be547
merchansky-alevel <e.merchansky@sloboda-studio.com>
2018-08-08 00:18:21 +0300

    feat: add validation functionality

--- CustomTaskRunner/TasksRepository/Validation/Validation.cs
+++ CustomTaskRunner/TasksRepository/Validation/Validation.cs
@@ -1,16 +1,36 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TasksRepository
 {
-    public class Validation
+    public class DataValidation
     {
-        public Validation()
-        {
+        public bool RunWithComplexity(string complexity) => CheckComplexity(complexity);
 
+        public bool RunWithName(string name) => CheckName(name);
+
+        private bool CheckName(string value)
+        {
+            return Length(value) > 1 ? true : WithMessage(false, "short name!");
         }
+
+        private bool CheckComplexity(string value)
+        {
+            return Length(value) > 0 ? Parser(value) : WithMessage(false, "invalid complexity!");
+        }
+
+        private bool Parser (string value)
+        {
+            value = value.IndexOf(".") != -1 ? value.Replace('.', ',') : value;
+
+            return Int32.TryParse(value, out int isInt) || Double.TryParse(value, out double isDouble);
+        }
+
+        private bool WithMessage(bool value, string message)
+        {
+            Console.WriteLine("ERROR: {0}", message);
+            return value;
+        }
+
+        private int Length(string value) => value.ToString().Length;
     }
 }
